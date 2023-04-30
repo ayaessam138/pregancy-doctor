674c2e5fb0feb081791e5e453dec85fb5877bbf5
nouranmohamed26 <nouranmohamed893@gmail.com>
2023-05-01 00:27:01 +0300

    main update

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,32 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:l10n_flutter/examples/models/l10n.dart';
+//import 'package:l10n_flutter/examples/models/l10n.dart';
 import 'package:pregancydoctor/cubit/calender_cubit/calender_cubit.dart';
 import 'package:pregancydoctor/cubit/pregancyform_cubit/pregancyform_cubit.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+//import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:pregancydoctor/housescrrens/addnotes.dart';
 import 'package:pregancydoctor/housescrrens/appointments.dart';
 import 'package:pregancydoctor/housescrrens/notess.dart';
 import 'package:pregancydoctor/housescrrens/patients.dart';
 import 'package:pregancydoctor/housescrrens/selectnurses.dart';
-import 'package:pregancydoctor/screens/calander.dart';
+//import 'package:pregancydoctor/screens/calander.dart';
 import 'package:pregancydoctor/screens/home_page.dart';
+import 'package:pregancydoctor/screens/loginScreen.dart';
 import 'package:pregancydoctor/screens/pregancyform.dart';
 import 'package:pregancydoctor/screens/profile.dart';
 
 import 'cubit/navgation_cubit/navgation_cubit/navgation_cubit.dart';
 
-import '../housescrrens/notes.dart';
+//import '../housescrrens/notes.dart';
 import 'firebase_options.dart';
-import 'package:pregancydoctor/l10n/class L10n.dart';
+//import 'package:pregancydoctor/l10n/class L10n.dart';
 
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+//import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 // dart pub global activate flutter_gen
-import 'package:easy_localization/easy_localization.dart';
+//import 'package:easy_localization/easy_localization.dart';
+
+import 'l10n/app_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -51,6 +54,7 @@ class pregancy extends StatelessWidget {
       child: MaterialApp(
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         routes: {
+          '/': (context) => Login(),
           homepage.homeid: (context) => homepage(),
           selectnurses.selectnursesid: (context) => selectnurses(),
           notess.notesid: (context) => notess(),
@@ -60,7 +64,7 @@ class pregancy extends StatelessWidget {
           profile.profileid: (context) => profile(),
           pregancyform.pregancyformid: (context) => pregancyform()
         },
-        initialRoute: homepage.homeid,
+        initialRoute: Login.logid,
       ),
     );
   }
